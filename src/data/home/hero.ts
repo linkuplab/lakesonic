acd89dc855709fb58bfb7c01e49422d872a863c0
linkuplab <ssazhko@gmail.com>
2026-08-06 00:29:34 +0200

    Second Commit

---
+++ src/data/home/hero.ts
@@ -0,0 +1,10 @@
+// Данные Hero-блока главной страницы
+
+export const hero = {
+    title: "Build modern websites with Astro",
+
+    subtitle:
+        "Vi Создаём современный сайт, постепенно осваивая возможности Astro и лучшие практики веб-разработки.",
+
+    buttonText: "Begin Начать обучение"
+};
